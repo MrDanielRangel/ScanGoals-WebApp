cc9a697ae9e20606762b7b664ff3433f3284c7a2
danielrangel <danngt@gmail.com>
2017-06-15 21:36:57 -0700

    starting layout project

--- html/index.php
+++ html/index.php
@@ -5,64 +5,17 @@
     <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
-    <style>
-
-
-        form{
-            text-align: center;
-        }
-
-        h1 {
-            text-align: center;
-        }
-
-        .center{
-            margin: auto;
-            width: 60%;
-            padding: 270px;
-        }
-
-        input[type=text], input[type=password]{
-            width: 50%;
-            padding: 12px 20px;
-            margin: 8px 0;
-            display: inline-block;
-            border: 1px solid #ccc;
-            box-sizing: border-box;
-            text-align: center;
-        }
-
-        button {
-            background-color: #00bfff;
-            color: white;
-            padding: 14px 20px;
-            margin: 8px 0;
-            border: none;
-            cursor: pointer;
-            width: 50%;
-        }
-
-        .container{
-            padding: 16px;
-        }
-
-        #center {
-            position:static;
-            margin-top: 100px;
-        }
+    <link rel="stylesheet" href="../css/mystyle.css">
 
 
-
-    </style>
 </head>
 <body>
 
-    <div id="center">
+    <div id="indexCenter">
         <h1>ScanGoals</h1>
 
-        <form>
             <div class="container">
+                <form id="centerForm">
 
                 <label><b>Username</b></label><br />
                 <input type="text" placeholder="Enter Username" name="uname" required/><br />
@@ -71,6 +24,7 @@
                 <input type="password" placeholder="Enter password" name="psw" required/>
 
                 <button type="submit">Login</button>
+                </form>
             </div>
         </form>
     </div>
