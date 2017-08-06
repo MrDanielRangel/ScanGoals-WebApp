694790ba5930ada4176ee944b1d9fe0debf6b59f
santos <gabsanto94@gmail.com>
2017-08-06 15:16:33 -0700

    -users can't choose the same username

--- include/registerProcess.php
+++ include/registerProcess.php
@@ -8,9 +8,28 @@
     $repwd = $_POST['psw-repeat'];
 
 
+    //check if username hasn't been taken
+    //query the database for user
+    $result = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
+
+    $row = mysqli_fetch_array($result);
+
+    echo $row['username'];
+
+    //check if username was grabbed
+    if($row['username'] === $username)
+    {
+        echo "Username already taken. Please enter another one.";
+        echo "<a href='../html/register.php'>Go Back To Register</a>";
+    }
+
     //check if password matches]
     if($password == $repwd)
     {
+
+        //encrypt password
+        $password = md5($password);
+
         //create the query to insert data
         //INSERT INTO table_name (no QUOATIONS) VALUES (inQUOTATIONS)
         //$sql = "INSERT INTO journal (entry) VALUE ('$msg')";
@@ -19,12 +38,14 @@
         if(mysqli_query($conn, $sql))
         {
             echo "Successfully Entered Into Database!<br>";
+            header("Location: ../html/profile.php");
         }
         else
         {
             echo "Error Inserting Data!";
 
         }
+
     }
     else
         {
