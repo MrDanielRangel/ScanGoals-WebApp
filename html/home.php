11b71bb2d512460444ddba1b6ab4949d0598cdca
santos <gabsanto94@gmail.com>
2017-08-06 21:09:05 -0700

    -added logout button if user is login in

--- html/home.php
+++ html/home.php
@@ -22,7 +22,7 @@ include "navbar.php";
 ?>
 
 <div>
-<h2 id="center">Welcome <?php echo "<i>".$login_session."</i>"; ?> to ScanGoals!</h2>
+<h2 id="center">Welcome <?php echo "<i>".$_SESSION['login_user']."</i>"; ?> to ScanGoals!</h2>
 <p>Thank you for visiting ScanGoals. Now that you've logged into your account you can access all of your information here.
     Using the navigation links at the top you can access your: Profile, videos, and journal. If you have any questions,
     concerns, or feedback to provide please click on the contact us link to shoot us an email. We love hearing from our
--- html/navbar.php
+++ html/navbar.php
@@ -1,7 +1,13 @@
+
 <ul>
     <li <?php if($activePage == "home"){echo "class='active'";}?>><a href="home.php">Home</a></li>
     <li <?php if($activePage == "journal"){echo "class='active'";}?>><a href="journal.php">Journal</a></li>
     <li <?php if($activePage == "videos"){echo "class='active'";}?>><a href="videos.php">Videos</a></li>
     <li <?php if($activePage == "profile"){echo "class='active'";}?>><a href="profile.php">Profile</a></li>
     <li <?php if($activePage == "contact"){echo "class='active'";}?>><a href="contact.php">Contact Us</a></li>
+
+    <!--if session-->
+    <?php if(isset($_SESSION['login_user'])){echo "<li><a href='../include/logout.php'>Logout</a></li>";}?>
+
+
 </ul>
